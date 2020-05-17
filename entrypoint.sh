d0f2e3a28e71fa9cf03737f7ab65ef3bf3364e7c
Liwei Ma <Liwei.Ma@sifive.com>
2020-05-18 01:44:02 +0800

    create release

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,4 @@ set -euxo pipefail
 cd workspace
 mkdir -p scala && wget -O scala/coursier https://git.io/coursier-cli && chmod +x scala/coursier
 wake --init .
-wake -x 'compileScalaModule rocketchipScalaModule'
-pwd
-ls
-exec echo $INPUT_ARGUMENTS
+exec wit $INPUT_ARGUMENTS
